789974f8c7bba32e2ba2c8b211f9029cf9d4d564
hemuhemant9630 <hemant_raghuwanshi@srmap.edu.in>
2024-10-23 19:22:44 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+const Index = () => {
+    return (
+        <View style={{
+            backgroundColor: "red",
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center"
+        }} 
+        >
+            <Text style={{ fontSize: 20, fontWeight: "700", color: "white" }} >hello welcome to shivam's app</Text>
+        </View>
+    );
+};
+
+export default Index;
